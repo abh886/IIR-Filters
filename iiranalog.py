a89da3b46c68782ce77c0ce618c647308ae2cea6
Abhinav Narayan <35916277+abh886@users.noreply.github.com>
2019-02-23 02:20:45 -0600

    Add files via upload

--- iiranalog.py
+++ iiranalog.py
@@ -11,7 +11,8 @@ fsa=int(input('S.B freq:'))
 fs=int(input('sampling freq:'))                           #To switch between Butterworth and Chebyshev filter 
 rp=int(input('passband attn:'))                         # uncomment/comment lines 25,27,29,31
 rs=int(input('stopband attn:'))
-
+                                                        #To change between lowpass and high pass
+                                                        #Replace 'low' with 'high' or vice versa
 n=linspace(0,79)
 x1=cos(2*pi*200*n/fs)   
 
@@ -22,13 +23,13 @@ wpd=2*pi*fpa/fs
 wsd=2*pi*fsa/fs
 
 
-N,wn=buttord(wpd,wsd,rp,rs,True)
+#N,wn=buttord(wpd,wsd,rp,rs,True)
 
-#N,wn=cheb1ord(wpd,wsd,rp,rs,True)
+N,wn=cheb1ord(wpd,wsd,rp,rs,True)
 
-#b,a=cheby1(N,rp,wn,'low',True)
+b,a=cheby1(N,rp,wn,'high',True)
 
-b,a=butter(N,wn,'low',True)
+#b,a=butter(N,wn,'high',True)
 
 
 
